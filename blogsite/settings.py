664236ce796e575f91b442d215300705ca81e383
qasimsyed823 <syedmqasimalibukhari@gmail.com>
2026-03-04 03:07:48 +0500

    Ready for Vercel + Supabase deployment

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -90,17 +90,19 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+supabase_db_url = os.environ.get("DATABASE_URL")
 
-supabase_db_url = os.environ.get('DATABASE_URL')
 if supabase_db_url:
-    DATABASES['default'] = dj_database_url.parse(supabase_db_url, conn_max_age=600)
-
+    DATABASES = {
+        'default': dj_database_url.parse(supabase_db_url, conn_max_age=600, ssl_require=True)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
